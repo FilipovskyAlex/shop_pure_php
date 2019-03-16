aba669c7662e1d7ed0cad82777e0db8dffb268c4
FilipovskyAlex <filipovskiy99@gmail.com>
2019-03-16 21:18:39 +0300

    create homepage

---
+++ views/site/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>My shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="../../template/css/index.css">
+    <!-- <script src="main.js"></script> -->
+</head>
+<body>
+<div class="container-fluid">
+    <div class="col-lg-12">
+        <header class="header">
+            <nav class="main-nav">
+                <ul class="nav">
+                    <li class="nav-item"><p class="logo"><a href="#"><img class="logo img-responsive" src="../../template/images/logo.png" alt="logo"></a></p></li>
+                    <li class="nav-item shopping-cart"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i>Корзина</a></li>
+                    <li class="nav-item account"><a class="nav-link" href="#"><i class="fas fa-user"></i>Аккаунт</a></li>
+                    <li class="nav-item login"><a class="nav-link" href="#"><i class="fas fa-lock"></i>Вход</a></li>
+                </ul>
+                <ul class="nav-meta">
+                    <li class="nav-item home"><a class="nav-link" href="#">Главная</a></li>
+                    <li class="nav-item shop dropdown">
+                        <a class="dropbtn" href="#">Магазин</a><i class="arrow fas fa-angle-down"></i>
+                        <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </li>
+                    <li class="nav-item blog"><a class="nav-link" href="#">Блог</a></li>
+                    <li class="nav-item about"><a class="nav-link" href="#">О магазине</a></li>
+                    <li class="nav-item contact"><a class="nav-link" href="#">Контакты</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="d-flex">
+            <div class="sidebar">
+                <p class="catalog">Каталог</p>
+                <ul class="listing-item">
+                    <li class="item"><a href="#">Категория</a></li>
+                    <li class="item"><a href="#">Категория</a></li>
+                    <li class="item"><a href="#">Категория</a></li>
+                    <li class="item"><a href="#">Категория</a></li>
+                    <li class="item"><a href="#">Категория</a></li>
+                </ul>
+            </div>
+            <div class="listing">
+                <p class="listing">Последние товары</p>
+                <div class="d-flex main">
+                    <div class="item">
+                        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+                        <div class="price">$120</div>
+                        <div class="name">Dress E-140-t</div>
+                        <div class="add"><a href="#"><i class="fas fa-shopping-cart"></i>В корзину</a></div>
+                    </div>
+                    <div class="item">
+                        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+                        <div class="price">$120</div>
+                        <div class="name">Dress E-140-t</div>
+                        <div class="add"><a href="#"><i class="fas fa-shopping-cart"></i>В корзину</a></div>
+                    </div>
+                    <div class="item">
+                        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+                        <div class="price">$120</div>
+                        <div class="name">Dress E-140-t</div>
+                        <div class="add"><a href="#"><i class="fas fa-shopping-cart"></i>В корзину</a></div>
+                    </div>
+                    <div class="item">
+                        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+                        <div class="price">$120</div>
+                        <div class="name">Dress E-140-t</div>
+                        <div class="add"><a href="#"><i class="fas fa-shopping-cart"></i>В корзину</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slider">
+
+        </div>
+        <footer class="footer d-flex">
+            <div class="facebook"><p><a href="#"><img class="img-responsive" alt="" src="../../template/images/facebook-app-symbol.png"></a></p></div>
+            <div class="google"><p><a href="#"><img class="img-responsive" alt="" src="../../template/images/social-google-plus-square-button.png"></a></p></div>
+            <div class="d-flex contacts">
+                <div class="phone">80292323953</div>
+                <div class="email">example@gmail.com</div>
+            </div>
+        </footer>
+    </div>
+</div>
+</body>
+</html>
