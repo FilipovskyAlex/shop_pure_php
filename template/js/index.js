8ae06e56f30f8b6ecc7b58a29c288a109ecf74bd
FilipovskyAlex <filipovskiy99@gmail.com>
2019-03-17 16:07:23 +0300

    add static slider

---
+++ template/js/index.js
@@ -0,0 +1,9 @@
+let slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide d-flex';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing d-flex';
+}
--- views/site/index.php
+++ views/site/index.php
@@ -1,30 +1,44 @@
 <?php include_once ROOT.'/views/layouts/header.php'?>
 
-        <div class="d-flex">
-            <div class="sidebar">
-                <p class="catalog">Каталог</p>
-                <ul class="listing-item">
-                    <?php foreach($categoryList as $categoryItem) :?>
-                    <li class="item"><a href="/category/<?= $categoryItem['id']?>"><?= $categoryItem['name']?></a></li>
-                    <?php endforeach; ?>
-                </ul>
+<div class="d-flex">
+    <div class="sidebar">
+        <p class="catalog">Каталог</p>
+        <ul class="listing-item">
+            <?php foreach($categoryList as $categoryItem) :?>
+            <li class="item"><a href="/category/<?= $categoryItem['id']?>"><?= $categoryItem['name']?></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div class="listing">
+        <p class="listing">Последние товары</p>
+        <div class="d-flex main">
+            <? foreach ($productList as $productItem): ?>
+            <div class="item">
+                <p class="item"><img src="../../template/<?= $productItem['image']?>" alt="dress"></p>
+                <div class="price">$<?= $productItem['price']?></div>
+                <div class="name"><a href="/product/<?= $productItem['id']?>"><?= $productItem['name']?></a></div>
+                <div class="add"><a href="#"><i class="fas fa-shopping-cart"></i>В корзину</a></div>
             </div>
-            <div class="listing">
-                <p class="listing">Последние товары</p>
-                <div class="d-flex main">
-                    <? foreach ($productList as $productItem): ?>
-                    <div class="item">
-                        <p class="item"><img src="../../template/<?= $productItem['image']?>" alt="dress"></p>
-                        <div class="price">$<?= $productItem['price']?></div>
-                        <div class="name"><a href="/product/<?= $productItem['id']?>"><?= $productItem['name']?></a></div>
-                        <div class="add"><a href="#"><i class="fas fa-shopping-cart"></i>В корзину</a></div>
-                    </div>
-                    <? endforeach; ?>
-                </div>
-            </div>
+            <? endforeach; ?>
         </div>
-        <div class="slider">
-
-        </div>
+    </div>
+</div>
+<div class="slider" id="slides">
+    <div class="slide showing d-flex">
+        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+        <p class="item"><img src="../../template/images/dress.jpg" alt="dress"></p>
+    </div>
+    <div class="slide d-flex">
+        <p class="item"><img src="../../template/images/astroworld_t-shirt.jpg" alt="dress"></p>
+        <p class="item"><img src="../../template/images/astroworld_t-shirt.jpg" alt="dress"></p>
+        <p class="item"><img src="../../template/images/astroworld_t-shirt.jpg" alt="dress"></p>
+    </div>
+    <div class="slide d-flex">
+        <p class="item"><img src="../../template/images/joggers-black.jpg" alt="dress"></p>
+        <p class="item"><img src="../../template/images/joggers-black.jpg" alt="dress"></p>
+        <p class="item"><img src="../../template/images/joggers-black.jpg" alt="dress"></p>
+    </div>
+</div>
 
 <?php include_once ROOT.'/views/layouts/footer.php'?>
